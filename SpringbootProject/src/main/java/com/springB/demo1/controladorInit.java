79a8011b666271fd1a3c9dc427f282fac6590c3d
Eliab Jaraba <114262159+EliabRicardoIS04@users.noreply.github.com>
2024-04-17 09:09:19 -0500

    commit 6 Eliab Jaraba

--- SpringbootProject/src/main/java/com/springB/demo1/controladorInit.java
+++ SpringbootProject/src/main/java/com/springB/demo1/controladorInit.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.springB.demo1.modelo.Usuario;
 
+ ;
 
 @Controller
 @Slf4j
@@ -19,6 +21,14 @@ public class controladorInit {
         String mensaje = "Hola mundo Spring MVC";
         modelo.addAttribute("mensaje",mensaje);
         modelo.addAttribute("dato",dato);
+        
+        Usuario u =new Usuario();
+        u.setCedula("1234");
+        u.setClave("cristina");
+        u.setNombre("mario luigi");
+        u.setEmail("mario@gamil.com");
+        modelo.addAttribute("alguien", u);
+        
         log.info("este programa se esta ejecutando");
         return "index";
     }
---
+++ SpringbootProject/src/main/java/com/springB/demo1/modelo/Usuario.java
@@ -0,0 +1,11 @@
+
+package com.springB.demo1.modelo;
+import lombok.Data;
+
+@Data
+public class Usuario {
+    private String cedula;
+    private String clave;
+    private String nombre;
+    private String email;
+}
