9f1799b84b8f102ef3ac17e1b49d0c5f95b618f4
Eliab Jaraba <114262159+EliabRicardoIS04@users.noreply.github.com>
2024-04-17 08:09:41 -0500

    commit 5

--- SpringbootProject/src/main/java/com/springB/demo1/controladorInit.java
+++ SpringbootProject/src/main/java/com/springB/demo1/controladorInit.java
@@ -2,8 +2,9 @@
 package com.springB.demo1;
 
 import lombok.extern.slf4j.Slf4j;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -11,8 +12,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Slf4j
 public class controladorInit {
     
+    @Value("$(index.mensaje)")
+    String dato;
     @GetMapping("/")
-    public String inicio(){
+    public String inicio(Model modelo){
+        String mensaje = "Hola mundo Spring MVC";
+        modelo.addAttribute("mensaje",mensaje);
+        modelo.addAttribute("dato",dato);
         log.info("este programa se esta ejecutando");
         return "index";
     }
