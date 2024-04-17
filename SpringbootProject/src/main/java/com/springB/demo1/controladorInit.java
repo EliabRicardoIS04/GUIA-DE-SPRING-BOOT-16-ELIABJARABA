551a63fce3236f1e18b2252fffe972e7e25880aa
Eliab Jaraba <114262159+EliabRicardoIS04@users.noreply.github.com>
2024-04-17 09:28:46 -0500

    commit 7 Eliab Jaraba

--- SpringbootProject/src/main/java/com/springB/demo1/controladorInit.java
+++ SpringbootProject/src/main/java/com/springB/demo1/controladorInit.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.springB.demo1.modelo.Usuario;
+import java.util.Arrays;
+import java.util.List;
 
  ;
 
@@ -28,7 +30,19 @@ public class controladorInit {
         u.setNombre("mario luigi");
         u.setEmail("mario@gamil.com");
         modelo.addAttribute("alguien", u);
+        Usuario u4 =new Usuario();
+        u.setCedula("1234");
+        u.setClave("cristina");
+        u.setNombre("mario luigi");
+        u.setEmail("mario@gamil.com");
+        Usuario u5 =new Usuario();
+        u.setCedula("1234");
+        u.setClave("cristina");
+        u.setNombre("mario luigi");
+        u.setEmail("mario@gamil.com");
         
+        List<Usuario> listaUsuario = Arrays.asList(u4,u5);
+        modelo.addAttribute("usuarios", listaUsuario );
         log.info("este programa se esta ejecutando");
         return "index";
     }
